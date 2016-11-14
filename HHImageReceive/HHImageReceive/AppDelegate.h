76f0c70e3d8bb89fd4c28d32d8c6190bac429fa9
HCL黄 <hclhuang@hclhuangdemacbook-pro.local>
2016-11-14 21:56:39 +0800

    初始化项目

---
+++ HHImageReceive/HHImageReceive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHImageReceive
+//
+//  Created by HCL黄 on 16/11/14.
+//  Copyright © 2016年 HCL黄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HHImageReceive/HHImageReceive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HHImageReceive
+//
+//  Created by HCL黄 on 16/11/14.
+//  Copyright © 2016年 HCL黄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
